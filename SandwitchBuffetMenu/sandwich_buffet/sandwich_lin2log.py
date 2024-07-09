e1e2abbb993d1b8a686a6117599e721f470fa6e5
arek <66903621+arekgetsgit@users.noreply.github.com>
2024-07-09 13:44:24 +0100

    Update sandwich_lin2log.py
    adde expression to operation pulldown menu so it switch according to the choice of parent node

--- SandwitchBuffetMenu/sandwich_buffet/sandwich_lin2log.py
+++ SandwitchBuffetMenu/sandwich_buffet/sandwich_lin2log.py
@@ -1,7 +1,7 @@
 # --------------------------------------------------------------
 #  sandwich_lin2log.py
-#  Version: 1.0.0
-#  Last Updated: 03.01.2024
+#  Version: 1.0.1
+#  Last Updated: 09.07.2024
 # --------------------------------------------------------------
 
 # --------------------------------------------------------------
@@ -21,12 +21,16 @@ def sandwichLin2Log():
 
     #create bottom log2lin
     lin2logbottom=nuke.createNode("Log2Lin",inpanel=False)
+    lin2logbottom.setYpos(lin2logbottom.ypos() + 100)
 
     # get names of created log2lin nodes
     ltname=lin2logtop.knob('name').getValue()
     lbname=lin2logbottom.knob('name').getValue()
 
     # set expression
+
+    lin2logbottom.knob('operation').setExpression(""+ltname+".operation == 1 ? 0 : 1 || "+ltname+"operation == 0 ? 1 : 1")
+
     lin2logbottom.knob('black').setExpression("parent."+ltname+".black")
     lin2logbottom.knob('white').setExpression("parent."+ltname+".white") 
     lin2logbottom.knob('gamma').setExpression("parent."+ltname+".gamma")
