712a039f4161240185a7805324af500cf3b4e2b3
arek <66903621+arekgetsgit@users.noreply.github.com>
2024-09-20 12:07:36 +0100

    Update sandwich_gamma.py
    setting channels to RGB only

--- SandwitchBuffetMenu/sandwich_buffet/sandwich_gamma.py
+++ SandwitchBuffetMenu/sandwich_buffet/sandwich_gamma.py
@@ -1,7 +1,7 @@
 # --------------------------------------------------------------
 #  sandwich_gamma.py
 #  Version: 1.0.0
-#  Last Updated: 03.01.2024
+#  Last Updated: 20.09.2024
 # --------------------------------------------------------------
 
 # --------------------------------------------------------------
@@ -19,8 +19,10 @@ def sandwichGamma():
 
     # create bottom gamma node
     gambottom=nuke.createNode('Gamma',inpanel=False)
+    gambottom.knob('channels').setValue('rgb')
     gambottom.knob('value').setValue(1.8)
     gambottom.knob('label').setValue('set values here')
+    gambottom.setYpos(gambottom.ypos() + 100)
 
     # get names of created gamma nodes
     gtname=gamtop.knob('name').getValue()
@@ -29,6 +31,7 @@ def sandwichGamma():
     # set expression 
     gamtop.knob('value').setExpression("1/parent."+gbname+".value")
     gamtop.knob('channels').setExpression("parent."+gbname+".channels")
+
     
 # Add menu items to the SandwichBuffet nodes menu
 nuke.menu('Nodes').addCommand("SandwichBuffet/sandwich_Gamma", "sandwich_gamma.sandwichGamma()") 
